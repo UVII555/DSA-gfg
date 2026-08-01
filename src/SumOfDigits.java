71f6322e117509ed84712db38882df261a7513d7
UVII555 <singhutsav555.com>
2026-08-01 12:57:33 +0530

    SumOfDigits.java

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -1,8 +1,15 @@
 public class SumOfDigits {
     static void main(String[] args) {
-        int x=257;
+        int x = 257;
+        int sum = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            sum += digit;
+            x = x / 10;
 
 
+        }
+        System.out.print(+sum);
     }
-}
 
+}
